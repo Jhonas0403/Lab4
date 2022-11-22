6655bcc32c9866d60dbd09fd62250cac96aea3a7
Jhonatan <acunahuisa@gmail.com>
2022-11-22 18:24:33 -0500

    change field tables

--- restaurant/models.py
+++ restaurant/models.py
@@ -9,13 +9,17 @@ class Project(models.Model):
 
 class Product(models.Model):
     name= models.CharField(max_length=200)
+    description = models.TextField(default = "")
+    
     def __str__(self) -> str:
         return self.name
 
 class Platos(models.Model):
     nombre = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(default = "")
     producto = models.ForeignKey(Product, on_delete = models.DO_NOTHING)
+    precio = models.FloatField(default=1)
+    cantidad = models.IntegerField(default = 1)
 
     def __str__(self) -> str:
         return self.nombre+'-'+self.producto.name
